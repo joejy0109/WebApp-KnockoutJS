91d31b73c5a811b7da8b49c0802da93f68c49378
joejy0109 <joejy0109@gmail.com>
2016-03-16 17:38:40 +0900

    Update dynamic-modal.js

--- WebApp-KnockoutJS/dynamic-modal.js
+++ WebApp-KnockoutJS/dynamic-modal.js
@@ -13,12 +13,14 @@ var dynamicModal = modal = {
         return JSON.stringify(data);
     },   
     modalObj: null,
+    confirmMessage: undefined,
     open: function (templateUrl, params, callback) {
         var $this = this;
         if ($.isFunction(params)) {
             callback = params;
             params = undefined;
         }
+        $("body").off("submit", "#modalLayer form");
         if ($("#modalLayer").length == 0) {
             $("body").append('<div id="modalLayer">'+
                                 '<div id="myModal" class="modal fade" role="dialog">' + 
@@ -37,17 +39,74 @@ var dynamicModal = modal = {
             if (xhr.status == 200) {
                 $.validator.unobtrusive.parse("#modalLayer form");
                 $.validator.methods.number = function (e) { return true; };
-                if (typeof callback === "function") {
-                    callback(res);
-                }                
                 $("#modalLayer div:eq(0)").modal('show');
+                if (typeof callback === "function") {
+                    callback($(this));
+                }
             } else {
                 alert(xhr.responseText);
             }
         });
         return this;
     },
+    confirm: function(confirmMessage) {       
+        this.confirmMessage = confirmMessage;
+        return this;
+    },
+
+
+    // test용
+    submit2: function(confirmMsg) {
+        return function (processContinue, params, success, fail) {
+            if (typeof processContinue !== "boolean") {
+                fail = success;
+                success = params;
+                params = processContinue;
+                processContinue = false;
+            }
+            if ($.isFunction(params)) {
+                fail = success;
+                success = params;
+                params = undefined;
+            }
+            var $this = this;
+
+            $("body").on("submit", "#modalLayer form", function (e) {
+                if (!processContinue)
+                    e.preventDefault();
+                if (confirmMsg != undefined) {
+                    if (!confirm(confirmMsg)) {
+                        return;
+                    }
+                }
+                var frm = $(this);
+                jQuery.ajax({
+                    url: frm.attr('action'),
+                    type: 'POST',
+                    dataType: 'json',
+                    data: $this.getFormData(frm, params),
+                    processData: false,
+                    contentType: 'application/json; charset=UTF-8',
+                    headers: {
+                        "__RequestVerificationToken": frm.find("input[name='__RequestVerificationToken']").val()
+                    },
+                }).done(function (res, status, xhr) {
+                    if ($.isFunction(success))
+                        success(res, $this.modalObj);
+                    $("#modalLayer div:eq(0)").modal('hide');
+                }).error(function (xhr, textStatus, errThrown) {
+                    //alert(textStatus);
+                }).fail(function (xhr, textStatus, errThrown) {
+                    if (textStatus == "parsererror") {
+                        $("#dyModalContent").html(xhr.responseText);
+                    }
+                });
+            });
+        };
+    },
+
     submit: function (processContinue, params, success, fail) {
+
         if (typeof processContinue !== "boolean") {
             fail = success;
             success = params;
@@ -62,8 +121,13 @@ var dynamicModal = modal = {
         var $this = this;
                 
         $("body").off("submit", "#modalLayer form").on("submit", "#modalLayer form", function (e) {
-            if (!processContinue) {
+            if (!processContinue)
                 e.preventDefault();
+            if ($this.confirmMessage != undefined) {
+                if (!confirm($this.confirmMessage)) {
+                    $this.confirmMessage = undefined;
+                    return;
+                }
             }
             var frm = $(this);
             jQuery.ajax({
