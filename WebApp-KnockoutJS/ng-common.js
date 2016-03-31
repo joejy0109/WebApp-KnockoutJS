c9f22d4772b6e2b4b5af2e28e74cc97102ac1bac
joejy0109 <joejy0109@gmail.com>
2016-03-31 13:46:13 +0900

    Update ng-common.js

--- WebApp-KnockoutJS/ng-common.js
+++ WebApp-KnockoutJS/ng-common.js
@@ -112,3 +112,12 @@ var $app = angular.module('myApp', [])
 
     };
 }]);
+
+// text highlighting
+$app.filter('highlight', ['$sce', function ($sce) {
+    return function (text, phrase) {
+        if (phrase)
+            text = text.replace(new RegExp('(' + phrase + ')', 'gi'), '<span style="background-color:yellow">$1</span>');
+        return $sce.trustAsHtml(text);
+    };
+}]);
