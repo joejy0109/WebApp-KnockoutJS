b8c6c54af7ba1ca49812a8ea6cbbcb6fe6b486a2
joejy0109 <joejy0109@gmail.com>
2016-04-05 16:08:39 +0900

    Update ValidateAntiForgeryTokenEx.cs

--- WebApp-KnockoutJS/ValidateAntiForgeryTokenEx.cs
+++ WebApp-KnockoutJS/ValidateAntiForgeryTokenEx.cs
@@ -8,7 +8,13 @@ using System.Web.Mvc;
 
 namespace MyWebAppPackage.Extensions
 {
-    public class ValidateAntiForgeryTokenEx : AuthorizeAttribute
+    /// <summary>
+    /// Cross-Site Request Forgery를 위한 토큰 검사기.
+    /// 
+    /// @author: Jeongyong, Jo
+    /// @date: 2016-02-24
+    /// </summary>
+    public sealed class ValidateAntiForgeryTokenExAttribute : AuthorizeAttribute
     {
         const string TOKEN_KEY = "__RequestVerificationToken";
 
@@ -21,20 +27,30 @@ namespace MyWebAppPackage.Extensions
                 {
                     var headerToken = req.Headers.Get(TOKEN_KEY);
                     if (headerToken == null)
-                        throw new InvalidOperationException("Header does not contain " + TOKEN_KEY);
+                        throw new HttpAntiForgeryException(string.Format("Header does not contain {0}", TOKEN_KEY));
 
-                    var tokenCookie = req.Cookies.Get(TOKEN_KEY);
-                    if (tokenCookie == null || tokenCookie.Value == null)
-                        throw new InvalidOperationException("Cookie does not contain " + TOKEN_KEY);
+                    var cookieToken = req.Cookies.Get(AntiForgeryConfig.CookieName);
+                    if (cookieToken == null || cookieToken.Value == null)
+                        throw new HttpAntiForgeryException(string.Format("Cookie does not contain {0}", TOKEN_KEY));
 
-                    AntiForgery.Validate(tokenCookie.Value, headerToken);
+                    AntiForgery.Validate(cookieToken.Value, headerToken);
                 }
                 else
                 {
                     new ValidateAntiForgeryTokenAttribute().OnAuthorization(filterContext);
                 }
             }
-        }
 
+            base.OnAuthorization(filterContext);
+        }
+    }
+
+    public class AntiForgeryConfigure
+    {
+        public static void Init(HttpContextBase ctx)
+        {
+            AntiForgeryConfig.UniqueClaimTypeIdentifier = System.Security.Claims.ClaimTypes.NameIdentifier;
+            //AntiForgeryConfig.AdditionalDataProvider.ValidateAdditionalData(ctx, "joejy");
+        }
     }
 }
