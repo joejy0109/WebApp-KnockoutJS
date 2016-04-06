d602bb3c99f9feae4bd77f3a8054320570303dfc
joejy0109 <joejy0109@gmail.com>
2016-04-06 14:12:07 +0900

    Update AuthorizeAttribute.cs

--- WebApp-KnockoutJS/AuthorizeAttribute.cs
+++ WebApp-KnockoutJS/AuthorizeAttribute.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace Citi.MyCitigoldFP.Common.Web.Auth
+namespace JOEJY
 {
     [XmlRoot("authroize")]
     public class Authorize
