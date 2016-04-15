35b9433299ae79339ae940d0bf18ad3e76d8755b
joejy0109 <joejy0109@gmail.com>
2016-04-15 16:55:57 +0900

    Update NoCacheControlAttribute.cs

--- WebApp-KnockoutJS/NoCacheControlAttribute.cs
+++ WebApp-KnockoutJS/NoCacheControlAttribute.cs
@@ -1,14 +1,28 @@
-[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-public class NoCacheControlAttribute : ActionFilterAttribute
-{
-    public override void OnActionExecuting(ActionExecutingContext filterContext)
-    {
-        filterContext.HttpContext.Response.Cache.SetExpires(DateTime.UtcNow.AddDays(-1));
-        filterContext.HttpContext.Response.Cache.SetValidUntilExpires(false);
-        filterContext.HttpContext.Response.Cache.SetRevalidation(HttpCacheRevalidation.AllCaches);
-        filterContext.HttpContext.Response.Cache.SetCacheability(HttpCacheability.NoCache);
-        filterContext.HttpContext.Response.Cache.SetNoStore();
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
 
-        base.OnActionExecuting(filterContext);
+namespace Citi.MyCitigoldFP.Web.Extensions.Filters
+{
+    /// <summary>
+    /// 브라우저 캐시 방지
+    /// @author: Jeongyong, Jo
+    /// @date: 2016-04-04
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class NoCacheControlAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            filterContext.HttpContext.Response.Cache.SetExpires(DateTime.UtcNow.AddDays(-1));
+            filterContext.HttpContext.Response.Cache.SetValidUntilExpires(false);
+            filterContext.HttpContext.Response.Cache.SetRevalidation(HttpCacheRevalidation.AllCaches);
+            filterContext.HttpContext.Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            filterContext.HttpContext.Response.Cache.SetNoStore();
+
+            base.OnActionExecuting(filterContext);
+        }
     }
 }
