2b6ab8ccf1a7e46c2413afd09531c7f552ce17f0
joejy0109 <joejy0109@gmail.com>
2016-05-25 10:25:15 +0900

    Create DateFormatString.cs

---
+++ WebApp-KnockoutJS/DateFormatString.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JOEJY
+{
+    public class DateFormatString : ValidationAttribute
+    {
+        public string Format { get; set; }
+
+        public DateFormatString(string format)
+        {
+            if (string.IsNullOrWhiteSpace(format))
+                throw new ArgumentNullException("format");
+
+            this.Format = format;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value.GetType() == typeof(string) && !string.IsNullOrWhiteSpace(value.ToString()))            
+            {
+                DateTime datetime;
+                if (!DateTime.TryParseExact(value.ToString(), Format, null, System.Globalization.DateTimeStyles.None, out datetime))
+                {
+                    return new ValidationResult(base.ErrorMessage);
+                }
+            }
+            return null;
+        }
+    }
+
+}
